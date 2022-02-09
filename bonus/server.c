316d6d284a0dbeee5ed840a2e0df8512f83ed295
NOGUEN <nhg1113@naver.com>
2022-02-10 01:41:29 +0900

    bonus almost done

--- bonus/server.c
+++ bonus/server.c
@@ -6,45 +6,55 @@
 /*   By: noguen <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 21:00:26 by noguen            #+#    #+#             */
-/*   Updated: 2022/02/10 01:27:41 by noguen           ###   ########.fr       */
+/*   Updated: 2022/02/09 00:34:18 by noguen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	encode(int sigusr, siginfo_t *info, void *tmp)
+void	bin_to_char(int sigusr)
 {
 	static char c = 0;
 	static int	i = 8;
 
-	(void)tmp;
 	if (sigusr == SIGUSR1)
-		c += (1 << --i);
-	else if (sigusr == SIGUSR2)
 		c |= (0 << --i);
+	else if (sigusr == SIGUSR2)
+		c |= (1 << --i);
 	if (i == 0)
 	{
+		if (c == '\0') {
+			write(1, "\n", 1);
+		}
 		write(1, &c, 1);
-		kill(info->si_pid, SIGUSR1);
 		i = 8;
 		c = 0;
 	}
 }
 
+void	handler(int sigusr)
+{
+	if (sigusr == SIGUSR1 || sigusr == SIGUSR2)
+		bin_to_char(sigusr);
+	else
+		exit(0);
+}
+
 int	main()
 {
-	struct sigaction	sig;
-
-	sig.sa_flags = SA_SIGINFO;
-	sig.sa_sigaction = &encode;
 	ft_putnbr(getpid());
 	write(1, "\n", 1);
-
-	while (1)
+	if (signal(SIGUSR1, handler) == SIG_ERR)
 	{
-		sigaction(SIGUSR1, &sig, NULL);
-		sigaction(SIGUSR2, &sig, NULL);
-		pause();
+		write(1, "Sigaction error.\n", 17);
+		exit(1);
 	}
+	if (signal(SIGUSR2, handler) == SIG_ERR)
+	{
+		write(1, "Sigaction error.\n", 17);
+		exit(1);
+	}
+	while (1)
+		;
 	return (0);
 }
--- client.c
+++ client.c
@@ -6,16 +6,15 @@
 /*   By: noguen <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 21:00:15 by noguen            #+#    #+#             */
-/*   Updated: 2022/02/09 00:34:09 by noguen           ###   ########.fr       */
+/*   Updated: 2022/02/10 01:34:23 by noguen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-char	*char_to_bin(int c)
+void	send(int pid, unsigned char c)
 {
 	int		bin[8];
-	char	*binary;
 	int		i;
 
 	i = -1;
@@ -27,93 +26,46 @@ char	*char_to_bin(int c)
 			bin[i] = 0;
 		c /= 2;
 	}
-	binary = malloc(sizeof(char) * 9);
 	i = -1;
 	while (++i < 8)
-		binary[i] = bin[7 - i] + 48;
-	binary[i] = '\0';
-	return (binary);
-}
-
-void	str_to_bin(t_client *client, char *bin)
-{
-	int	i;
-
-	i = -1;
-	client->bin_msg = malloc(sizeof(char *) * (client->len + 2));
-	while (++i < client->len + 2)
-		client->bin_msg[i] = NULL;
-	i = -1;
-	while (bin[++i])
-		client->bin_msg[i] = char_to_bin((int)bin[i]);
-	client->bin_msg[i] = char_to_bin(0);
-	client->bin_msg[i + 1] = NULL;
-}
-
-void	send_sig(t_client *client, int sigusr)
-{
-	int	tmp;
-
-	if (sigusr == 1)
 	{
-		tmp = kill(client->pid, SIGUSR1);
-		if (tmp == -1)
-		{
-			exit_client(client);
-			write(1, "Can't send siganl.\n", 19);
-			exit(0);
-		}
-	}
-	else if (sigusr == 2)
-	{
-		tmp = kill(client->pid, SIGUSR2);
-		if (tmp == -1)
-		{
-			write(1, "Can't send siganl.\n", 19);
-			exit(0);
-		}
+		if (bin[7 - i] == 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(300);
 	}
 }
 
-void	send_msg(t_client *client, char *msg)
+void	handler(int sig)
 {
-	int	i;
-	int	j;
-
-	i = -1;
-	str_to_bin(client, msg);
-	while (client->bin_msg[++i])
+	if (sig == SIGUSR1)
 	{
-		j = - 1;
-		usleep(30);
-		while (++j < 8)
-		{
-			usleep(30);
-			if (client->bin_msg[i][j] == '0')
-				send_sig(client, 1);
-			else if (client->bin_msg[i][j] == '1')
-				send_sig(client, 2);
-		}
+		write(1, "Server got message.\n", 20);
 	}
 }
-
 
 int	main(int argc, char *argv[])
 {
-	t_client	client;
+	struct sigaction	sig;
+	long int			pid;
+	int					i;
 
 	if (argc != 3)
 	{
 		write(1, "Incorrect parameters.\n", 22);
-		exit_client(&client);
 	}
+	sig.sa_handler = &handler;
 	if (!ft_isnum(argv[1]))
 	{
-		write(1, "123", 3);
 		return (0);
 	}
-	client.pid = ft_atoi(argv[1]);
-	client.len = ft_strlen(argv[2]);
-	send_msg(&client, argv[2]);
-	exit_client(&client);
+	pid = ft_atoi(argv[1]);
+	i = -1;
+	sigaction(SIGUSR1, &sig, NULL);
+	while (argv[2][++i])
+		send(pid, argv[2][i]);
+	while (1)
+		pause();
+	return (0);
 }
